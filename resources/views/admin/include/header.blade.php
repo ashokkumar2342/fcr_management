1916a65df0288dec0ee5765bc2674740c104f2a4
ashokkumar2342 <ashokkumar2342@gmail.com>
2023-04-23 15:08:47 +0530

    Update header.blade.php

--- resources/views/admin/include/header.blade.php
+++ resources/views/admin/include/header.blade.php
@@ -1,36 +1,45 @@
 
 @php
 $admin=Auth::guard('admin')->user();
+$notifications = DB::select(DB::raw("select * from `notifications` where `notification_for` = '$admin->id';"));
+ 
 @endphp 
-  <nav class="main-header navbar navbar-expand navbar-info navbar-light" style="background-color:#001f3f">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars" style="color:#fff"></i></a>
-      </li>
-          
-    </ul>
-    
-    <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-        
-        <h5 style="color:#fff;margin-top:10px">Welcom : {{$admin->first_name}}</h5>
-      
-      </li>       
-      <li class="nav-item">
-        <a class="btn btn-lg" title="Sign Out" href="{{ route('admin.logout.get') }}"
-                        onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-          <i class="fa fa-sign-out" style="color:#fff"></i>
-        </a>
-        <form id="logout-form" action="{{ route('admin.logout.get') }}" method="POST" style="display: none;">
-           {{ csrf_field() }}
-        </form>
-      </li>
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light"> 
+  <ul class="navbar-nav ml-auto"> 
 
-      
-          
+  <li class="nav-item dropdown" style="padding-top: 5px;">
+  <a class="nav-link" data-toggle="dropdown" href="#">
+  <i class="far fa-bell"></i>
+  <span class="badge badge-warning navbar-badge">{{ count($notifications) }}</span>
+  </a>
+  <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+  <span class="dropdown-item dropdown-header">15 Notifications</span>
+  <div class="dropdown-divider"></div>
+
+  @foreach ($notifications as $notification)
+    <a href="#" class="dropdown-item">
+  <i class="fas fa-envelope mr-2"></i> {{ $notification->remarks }}
+  <span class="float-right text-muted text-sm">3 mins</span>
+  </a>
+  @endforeach
+  
    
-    </ul>
+  <div class="dropdown-divider"></div>
+  <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+  </div>
+  </li>
+   
+  <li class="nav-item">
+     <a class="btn btn-lg" title="Sign Out" href="{{ route('admin.logout.get') }}"
+                     onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+       <i class="fa fa-sign-out" style="color:#21252;"></i>
+     </a>
+     <form id="logout-form" action="{{ route('admin.logout.get') }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+     </form>
+   </li>
+  </ul>
   </nav>
+
   <!-- /.navbar -->
